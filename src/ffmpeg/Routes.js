4ccf7d5924d031c4257572af705d0c2db253b9e2
Samrat <samratdot@outlook.com>
2021-04-04 15:42:20 +0530

    upload form design done

--- src/ffmpeg/Routes.js
+++ src/ffmpeg/Routes.js
@@ -14,18 +14,21 @@ export default function Routes(props) {
     return (
         <>
             <Route path="/" exact>
-                <Entry />
+                {
+                    //  <Entry />
+                    <Upload />
+                }
             </Route>
             <Route path="/admin" component={Admin} />
             <Switch>
                 <Route path="/home" exact component={Home} />
                 <Route path="/admin/upload" exact component={Upload} />
-                <Route path="/admin/conversion" exact render={() => { return <h3>Start Conversion</h3>}} />
-                <Route path="/admin/playfiles" exact render={() => { return <h3>Play Conversion Files</h3>}} />
-                <Route path="/admin/addcls" exact render={() => { return <h3>add Class</h3>}} />
-                <Route path="/admin/addsub" exact render={() => { return <h3>add Subject</h3>}} />
-                <Route path="/admin/addchap" exact render={() => { return <h3>add Chapter</h3>}} />
-                <Route path="/admin/mapping" exact render={() => { return <h3>Mapping Chapter - Video</h3>}} />
+                <Route path="/admin/conversion" exact render={() => { return <h3>Start Conversion</h3> }} />
+                <Route path="/admin/playfiles" exact render={() => { return <h3>Play Conversion Files</h3> }} />
+                <Route path="/admin/addcls" exact render={() => { return <h3>add Class</h3> }} />
+                <Route path="/admin/addsub" exact render={() => { return <h3>add Subject</h3> }} />
+                <Route path="/admin/addchap" exact render={() => { return <h3>add Chapter</h3> }} />
+                <Route path="/admin/mapping" exact render={() => { return <h3>Mapping Chapter - Video</h3> }} />
             </Switch>
         </>
     );

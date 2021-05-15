007fe695aef149d7cc0e29cf676a564efe12ea25
Samrat <samratdot@outlook.com>
2021-05-15 20:53:50 +0530

    add style to menu

--- src/ffmpeg/Admin/Admin.js
+++ src/ffmpeg/Admin/Admin.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './Admin.scss';
+import BrandAnimation from './BrandAnimation/BrandAnimation';
 import Menubar from './Menubar/Menubar';
 import { Toast } from './Toast/Toast';
 
@@ -17,6 +18,10 @@ export default function Admin() {
         <div className="C_Admin">
             <Menubar />
             <Toast />
+            <div style={{ position: 'relative' }}>
+                <BrandAnimation />
+            </div>
+            
         </div>
     )
 }
---
+++ src/ffmpeg/Admin/BrandAnimation/BrandAnimation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './BrandAnimation.scss';
+
+function BrandAnimation() {
+    return (
+        <div className="BrandAnimation">
+        </div>
+    )
+}
+
+export default BrandAnimation

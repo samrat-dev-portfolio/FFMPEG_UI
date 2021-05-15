358666a90f25698c99b74545efabe1e3b75a236b
Samrat <samratdot@outlook.com>
2021-05-15 20:08:27 +0530

    Setting ui done

--- src/ffmpeg/Admin/Settings/Settings.js
+++ src/ffmpeg/Admin/Settings/Settings.js
@@ -14,10 +14,41 @@ export default function Settings() {
     const [havepassword, setHavepassword] = useState(false);
 
     useEffect(() => {
-
+        SQliteHavePassword();
     }, []);
+    const SQliteHavePassword = () => {
+        setIsLoading(true);
+        axios.get(`${baseurl}api/mpeg/SQliteHavePassword`)
+            .then(res => {
+                setHavepassword(res.data.data);
+                setIsLoading(false);
+                // console.log(res.data);
+            }).catch(err => {
+                setError('Error load SQliteHavePassword');
+                setIsLoading(false);
+            });
+    };
 
-    const handleSubmit = () => { }
+    const handleSubmit = type => {
+        const _params = {
+            secret,
+            type
+        };
+        setIsLoading(true);
+        axios.get(`${baseurl}api/mpeg/SQliteChangePassword`, {
+            params: _params
+        })
+            .then(res => {
+                setIsLoading(false);
+                // console.log(res.data.data);
+                setError(res.data.data);
+                SQliteHavePassword();
+                setSecret('');
+            }).catch(err => {
+                setError('Error');
+                setIsLoading(false);
+            });
+    }
 
     return (
         <Container fluid className="C_Settings">
@@ -38,20 +69,20 @@ export default function Settings() {
                             }
                         </Card.Header>
                         <Card.Body>
-                            <Form noValidate onSubmit={handleSubmit}>
+                            <Form noValidate>
                                 <Form.Group controlId="Secret">
                                     <Form.Label>Secret</Form.Label>
                                     <Form.Control type="text" autoComplete="off" placeholder="Secret" required
-                                        defaultValue={secret}
+                                        value={secret}
                                         onChange={(e) => { setSecret(e.target.value); }} />
                                 </Form.Group>
                                 {
                                     havepassword ?
-                                        <MDBBtn type="submit" color="indigo" size="md" >
+                                        <MDBBtn type="submit" color="indigo" size="md" onClick={() => handleSubmit('remove')}>
                                             <MDBIcon icon="lock-open" size="2x" />
                                     &nbsp;&nbsp;Remove Password
                                 </MDBBtn> :
-                                        <MDBBtn type="submit" color="indigo" size="md">
+                                        <MDBBtn type="submit" color="indigo" size="md" onClick={() => handleSubmit('add')}>
                                             <MDBIcon icon="unlock" size="2x" />
                                 &nbsp;&nbsp;Add Password
                             </MDBBtn>

3a1176b5a06b280d22dd0b96ac105a82e0e37d68
Samrat <samratdot@outlook.com>
2021-06-15 00:08:03 +0530

    .

--- src/ffmpeg/Admin/Activation/KeyGenerator.js
+++ src/ffmpeg/Admin/Activation/KeyGenerator.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Button, Col, Container, Form, Row, Card, ProgressBar } from "react-bootstrap";
+import { Button, Col, Container, Form, Row, Card, Table, Pagination, Dropdown, ButtonGroup, InputGroup, FormControl } from "react-bootstrap";
 import Loading from '../Loading/Loading';
 import { MDBIcon, MDBBtn } from "mdbreact";
 import { ToastAlert } from '../../Admin/Toast/Toast';
@@ -18,9 +18,29 @@ export default function KeyGenerator() {
   const [getCopyKeyGen, setCopyKeyGen] = useState('');
   const [disabledRegister, setDisabledRegister] = useState(false);
 
+  const [getAllKeyGens, setAllKeyGens] = useState([]);
+  const [getKeyGenParams, setKeyGenParams] = useState({
+    "pageindex": 0,
+    "limit": 5
+  });
+  const [getPageItems, setPageItems] = useState([]);
+  const Limitoptions = [
+    { value: '5', label: '5' },
+    { value: '10', label: '10' },
+    { value: '50', label: '50' },
+    { value: '100', label: '100' }
+  ];
+  const [getCardBodyShow, setCardBodyShow] = useState(true);
+
+  let LazyKeyupTimer = useRef(null);
+  let appIDInput = useRef(null);
+  let SKeyInput = useRef(null);
+  let ToggleTarget = useRef(null);
+
   //#region Hooks 
   useEffect(() => {
     loadAppID();
+    LoadAllKeyGens(getKeyGenParams);
   }, []);
 
   //#endregion
@@ -39,7 +59,9 @@ export default function KeyGenerator() {
         setIsLoading(false);
       });
   };
-
+  const today = () => {
+    return moment().local().format('YYYY-MM-DD HH:mm:ss');
+  }
   const copyKeyGen = () => {
     // console.log(getCopyKeyGen);
     var textArea = document.createElement("textarea");
@@ -55,6 +77,8 @@ export default function KeyGenerator() {
       // console.error('Fallback: Oops, unable to copy', err);
     }
     document.body.removeChild(textArea);
+    ToastAlert('Copied', 's');
+
   }
   const handleSubmit = () => {
     const config = {
@@ -62,7 +86,7 @@ export default function KeyGenerator() {
         'content-type': 'application/json',
       }
     };
-    const body = { LicenceAppId: getAppId, LicenceKey: getSerialKey };
+    const body = { LicenceAppId: getAppId, LicenceKey: getSerialKey, creationDate: today() };
     setIsLoading(true);
     axios.post(`${baseurl}api/mpeg/AddLicenseKeyGen`, body, config).then(res => {
       // console.log(res);
@@ -72,9 +96,85 @@ export default function KeyGenerator() {
         ToastAlert(res.data.error, 'e');
       setIsLoading(false);
       setDisabledRegister(true);
+      LoadAllKeyGens(getKeyGenParams);
     }).catch(err => {
       console.log(err);
       setIsLoading(false);
+    });
+  }
+  const LoadAllKeyGens = (_params) => {
+    setIsLoading(true);
+    axios.get(`${baseurl}api/mpeg/getLicenseKeyGens`, {
+      params: _params
+    }).then(res => {
+      setAllKeyGens(res.data.data);
+      let { pageindex, totalPage } = res.data;
+      let PageItems = [];
+      for (let p = 0; p < totalPage; p++) {
+        PageItems.push(<Pagination.Item key={p} onClick={() => { pageChange(p) }} active={p == pageindex}>{p + 1}</Pagination.Item>);
+      }
+      setPageItems(PageItems);
+      setIsLoading(false);
+      // setError('');
+      // console.log(res.data);
+    }).catch(err => {
+      setIsLoading(false);
+      setError('Error loading Content');
+    });
+  }
+  const LazyKeyup = (_callback) => {
+    clearTimeout(LazyKeyupTimer.current);
+    LazyKeyupTimer.current = setTimeout(() => {
+      _callback();
+      // _callback.call(this, _args);
+    }, 1000);
+  };
+  const searchByAppID = _appID => {
+    SKeyInput.current.value = '';
+    LazyKeyup(() => {
+      setKeyGenParams(prevData => {
+        let data = { ...prevData, "appId": _appID, "pageindex": 0 };
+        delete data["serialKey"];
+        LoadAllKeyGens(data);
+        return data;
+      });
+    });
+  };
+  const searchBySKey = _SKey => {
+    appIDInput.current.value = '';
+    LazyKeyup(() => {
+      setKeyGenParams(prevData => {
+        let data = { ...prevData, "serialKey": _SKey, "pageindex": 0 };
+        delete data["appId"];
+        LoadAllKeyGens(data);
+        return data;
+      });
+    });
+  };
+  const pageChange = p_index => {
+    setKeyGenParams(prevData => {
+      let data = { ...prevData, "pageindex": p_index };
+      LoadAllKeyGens(data);
+      return data;
+    });
+  };
+  const toggleCardBody = () => {
+    setCardBodyShow(prev => {
+      let elm = ToggleTarget.current;
+      if (!elm) return;
+      let h = elm.offsetHeight;
+      if (prev) {
+        elm.classList.add('minimize-zone-active')
+        setTimeout(() => {
+          elm.style.marginTop = `-${h}px`;
+        }, 100);
+      } else {
+        elm.style.marginTop = `0px`;
+        setTimeout(() => {
+          elm.classList.remove('minimize-zone-active')
+        }, 300);
+      }
+      return !prev;
     });
   }
 
@@ -89,25 +189,80 @@ export default function KeyGenerator() {
               {
                 getIsLoading ? <Loading /> : null
               }
+              <span className="minimize-card" onClick={toggleCardBody}>
+                <MDBIcon icon={getCardBodyShow ? 'minus' : 'plus'} />
+              </span>
             </Card.Header>
-            <Card.Body>
-              <Form noValidate onSubmit={handleSubmit}>
-                <Form.Group controlId="App_ID">
-                  <Form.Label>App ID</Form.Label>
-                  <MDBIcon icon="redo-alt" className="indigo-text refresh-btn" size="lg" title="Refresh KeyGen" onClick={loadAppID} />
-                  <MDBIcon icon="copy" className="green-text refresh-btn" size="lg" title="Copy KeyGen" onClick={copyKeyGen} />
-                  <Form.Control type="text" defaultValue={getAppId} placeholder="App ID" readOnly />
-                </Form.Group>
-                <Form.Group controlId="Serial_Key">
-                  <Form.Label>Serial Key</Form.Label>
-                  <Form.Control type="text" defaultValue={getSerialKey} placeholder="Serial Key" readOnly />
-                </Form.Group>
-                <MDBBtn type="button" color="indigo" size="sm" onClick={handleSubmit} disabled={disabledRegister}>
-                  <MDBIcon icon="save" />&nbsp;&nbsp;Register
-                </MDBBtn>
-              </Form>
-            </Card.Body>
+            {
+              <Card.Body ref={ToggleTarget} className="minimize-zone">
+                <Form noValidate onSubmit={handleSubmit}>
+                  <Form.Group controlId="App_ID">
+                    <Form.Label>App ID</Form.Label>
+                    <MDBIcon icon="redo-alt" className="indigo-text refresh-btn" size="lg" title="Refresh KeyGen" onClick={loadAppID} />
+                    <MDBIcon icon="copy" className="green-text refresh-btn" size="lg" title="Copy KeyGen" onClick={copyKeyGen} />
+                    <Form.Control type="text" defaultValue={getAppId} placeholder="App ID" readOnly />
+                  </Form.Group>
+                  <Form.Group controlId="Serial_Key">
+                    <Form.Label>Serial Key</Form.Label>
+                    <Form.Control type="text" defaultValue={getSerialKey} placeholder="Serial Key" readOnly />
+                  </Form.Group>
+                  <MDBBtn type="button" color="indigo" size="sm" onClick={handleSubmit} disabled={disabledRegister}>
+                    <MDBIcon icon="save" />&nbsp;&nbsp;Register
+                  </MDBBtn>
+                </Form>
+              </Card.Body>
+            }
           </Card>
+        </Col>
+        <Col className="col-12 pt-3">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>
+                  <div className="td-filter-box">
+                    App ID
+                    <InputGroup size="sm" className="">
+                      <FormControl
+                        placeholder="Search by App ID"
+                        aria-label=""
+                        aria-describedby="basic-addon2"
+                        onKeyUp={(e) => { searchByAppID(e.target.value); }}
+                        ref={appIDInput}
+                      />
+                    </InputGroup>
+                  </div>
+                </th>
+                <th>
+                  <div className="td-filter-box">
+                    Serial Key
+                    <InputGroup size="sm" className="">
+                      <FormControl
+                        placeholder="Search by serial Key"
+                        aria-label=""
+                        aria-describedby="basic-addon2"
+                        onKeyUp={(e) => { searchBySKey(e.target.value); }}
+                        ref={SKeyInput}
+                      />
+                    </InputGroup>
+                  </div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                getAllKeyGens.map((item, index) => {
+                  return <tr key={index}>
+                    <td>{item.appId}</td>
+                    <td>{item.serialKey}</td>
+                    <td>{item.creationDate}</td>
+                  </tr>;
+                })
+              }
+            </tbody>
+          </Table>
+        </Col>
+        <Col className="col-8 pt-3 pagination-wrapper">
+          <Pagination size="sm">{getPageItems}</Pagination>
         </Col>
       </Row>
     </Container>

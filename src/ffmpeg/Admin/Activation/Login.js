6f02e354281aa4bcbdbbbaf6a542616beac6d97f
Samrat <samratdot@outlook.com>
2021-07-07 05:01:54 +0530

    activation

--- src/ffmpeg/Admin/Activation/Login.js
+++ src/ffmpeg/Admin/Activation/Login.js
@@ -10,7 +10,6 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
-import Dexie from 'dexie';
 import {
     GetFFMPEGLocalData,
     GetFFMPEGLocalDataByKey,
